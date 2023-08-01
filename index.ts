3c2f658e7fc245c061601d18892bab4fa664d634
choq94 <whrbdnjs1994@gmail.com>
2023-08-01 20:45:30 +0900

    feat: 배열에 나누어 담기

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
+// 객체로 생각하기
 const soluition = (name, yearning, photo) => {
   return photo.map((v) =>
     v.reduce((a, c) => {
@@ -5,3 +6,8 @@ const soluition = (name, yearning, photo) => {
     })
   );
 };
+
+// 분해해서 string 배열화
+// v = 'qwe'
+// [v] = ['qwe']
+// [...v] = ['q','w','e'];
