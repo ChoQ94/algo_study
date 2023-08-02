115ff8df5fbeba08d5f2149a0d2692552c515d19
choq94 <whrbdnjs1994@gmail.com>
2023-08-02 22:01:45 +0900

    feat: 그리다

--- index.ts
+++ index.ts
@@ -27,3 +27,15 @@ function solution(cards1, cards2, goal) {
 
   return 'Yes';
 }
+
+function solution2(n, m, sections) {
+  var answer = 0;
+  var painted = 0;
+  for (var section of sections) {
+    if (painted < section) {
+      answer++;
+      painted = section + m - 1;
+    }
+  }
+  return answer;
+}
