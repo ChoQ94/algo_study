60dce57873690da806aabd86d4ecb757bb961222
choq94 <whrbdnjs1994@gmail.com>
2023-08-02 21:01:51 +0900

    feat: 배열에 비교

--- index.ts
+++ index.ts
@@ -11,3 +11,19 @@ const soluition = (name, yearning, photo) => {
 // v = 'qwe'
 // [v] = ['qwe']
 // [...v] = ['q','w','e'];
+
+// 두 배열에 첫번째 값이 있는 지 확인
+// 숫자를 늘릴 필요 없이 배열을 shift()로 지워버리기.
+function solution(cards1, cards2, goal) {
+  for (const s of goal) {
+    if (cards1[0] == s) {
+      cards1.shift();
+    } else if (cards2[0] == s) {
+      cards2.shift();
+    } else {
+      return 'No';
+    }
+  }
+
+  return 'Yes';
+}
